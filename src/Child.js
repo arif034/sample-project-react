56548826c0c5d4bebee6c98caeb3c0ed31768e1a
arif034 <45455917+arif034@users.noreply.github.com>
2021-12-16 17:56:37 +0530

    Oo click toggle On click toggle code in Child.js class

--- src/Child.js
+++ src/Child.js
@@ -1,7 +1,34 @@
 import { Component } from "react";
 class Child extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "Guest User",
+      loggedIn: true
+    };
+    this.onButtonClick = this.onButtonClick.bind(this);
+  }
+
+  onButtonClick() {
+    if (this.state.loggedIn)
+      this.setState({
+        message: "Guest User",
+        loggedIn: false
+      });
+    else
+      this.setState({
+        message: "Arif",
+        loggedIn: true
+      });
+  }
   render() {
-    return <div>Hello World</div>;
+    return (
+      <div>
+        Hello {this.state.message}
+        <br />
+        <button onClick={this.onButtonClick}> Click Me </button>
+      </div>
+    );
   }
 }
 export default Child;
