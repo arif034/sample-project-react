d0981dbb4ad348cabfd9b421d72bb402cbbf0107
arif034 <45455917+arif034@users.noreply.github.com>
2021-12-17 12:24:37 +0530

    sample commenting sample commenting

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import "./styles.css";
 import { Component } from "react";
 //import Parent from './Parent.js'
 // import Child from './Child.js'
-import Parent2 from "./Parent2.js";
+// import Parent2 from "./Parent2.js";
 class App extends Component {
   render() {
     return (
       <div>
         {/* <Child /> */}
         {/* <Parent /> */}
-        <Parent2 />
+        {/* <Parent2 /> */}
       </div>
     );
   }
